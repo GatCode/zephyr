8c18dba83e858a86cad36b290dd368473b798aac
GatCode <gatcode@wdw.one>
2022-03-19 14:34:58 +0100

    Finish 8bit IO Coder

--- samples/gatcode/8bit_gpio/include/io_coder.h
+++ samples/gatcode/8bit_gpio/include/io_coder.h
@@ -14,119 +14,69 @@ struct io_coder {
 	 * @brief device* containing the port.
 	 */
 	const struct device *port;
+	/**
+	 * @brief gpio_port_pins_t containing all 8 pin definitions.
+	 */
+	gpio_port_pins_t pins;
 };
 
 /**
- * @brief Set specific GPIO port
- *
- * This function sets the io_coder port to the given port.
- *
- * @param coder   	  io_coder object.
- * @param port    	  Pointer to a GPIO port.
- *
- * @return Zero on success or (negative) error code otherwise.
- */
-int set_port(struct io_coder *coder, const struct device *port);
-
-/**
- * @brief Pin Setup
- *
- * This function performs the setup of a given pin.
- *
- * @param coder   	  io_coder object.
- * @param pin    	  Pin number of the MCU.
- * @param read        Toggle for the read mode configuration.
- *
- * @return Zero on success or (negative) error code otherwise.
- */
-int setup_pin(struct io_coder *coder, uint8_t pin, bool read);
-
-/**
- * @brief Pin setup of 8 given pins for port 1
+ * @brief Pin setup of 8 specified pins for port 0
  * 
- * This function performs the setup of 8 consecutive pins
- * of port 1 in read or write mode.
+ * This function is simmilar to setup_8_bit_io with first_pin and last_pin
+ * but allows for specific pin declaration.
  *
  * @param coder   	  io_coder object.
- * @param first_pin   Pin number first pin.
- * @param last_pin    Pin number first pin.
+ * @param pins	      gpio_port_pins_t containing 8 pin definitions
+ * @param port1   	  Port 1 configuration flag.
  * @param read   	  Read mode configuration flag.
  *
  * @return Zero on success or (negative) error code otherwise.
  */
-int setup_8_bit_io(struct io_coder *coder, uint8_t first_pin, uint8_t last_pin, bool read);
+int setup_8_bit_io(struct io_coder *coder, uint8_t pins[8], bool port1, bool read);
 
 /**
- * @brief Read 8-bit value of the given pin range
+ * @brief Pin setup of 8 given pins for port 0
  * 
- * This function performs a read operation of the given pin range
- * on the port specified in the io_coder object. The result contains
- * the 8-bit representation of of the pin states.
+ * This function performs the setup of 8 consecutive pins
+ * of port 0 in read or write mode. If the port1 flag is
+ * set, the setup will cover the specified pins on port 1.
  *
  * @param coder   	  io_coder object.
  * @param first_pin   Pin number first pin.
  * @param last_pin    Pin number first pin.
+ * @param port1   	  Port 1 configuration flag.
+ * @param read   	  Read mode configuration flag.
+ *
+ * @return Zero on success or (negative) error code otherwise.
+ */
+int setup_8_bit_io_consecutive(struct io_coder *coder, uint8_t first_pin, uint8_t last_pin, bool port1, bool read);
+
+/**
+ * @brief Read 8-bit value
+ * 
+ * This function performs a read operation of the set pin range
+ * and port specified in the io_coder object. The result contains
+ * the 8-bit representation of the pin states.
+ *
+ * @param coder   	  io_coder object.
  * @param result      Pointer in which the read value will be stored.
  *
  * @return Zero on success or (negative) error code otherwise.
  */
-int read_8_bit(struct io_coder *coder, uint8_t first_pin, uint8_t last_pin, uint8_t *result);
+int read_8_bit(struct io_coder *coder, uint8_t *result);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// /**
-//  * @brief Read 8-bit value of the pins 1-8 from a given port
-//  *
-//  * This function performs a read operation of
-//  * pins 1-8 of the given port. The result contains the 8-bit
-//  * representation of these pins.
-//  *
-//  * @param port   	  Port of the MCU.
-//  * @param result      Pointer in which the read value will be stored.
-//  *
-//  * @return Zero on success or (negative) error code otherwise.
-//  */
-// int read_P01_to_P08_from_port(const struct device *port, uint8_t *result);
-
-// /**
-//  * @brief Read 8-bit value of P0.01 - P0.08
-//  *
-//  * This function performs a read operation of
-//  * pins 1-8 of port 0. The result contains the 8-bit
-//  * representation of these pins.
-//  *
-//  * @param coder   	  io_coder object.
-//  * @param result      Pointer in which the read value will be stored.
-//  *
-//  * @return Zero on success or (negative) error code otherwise.
-//  */
-// int read_P0_01_to_P0_08(struct io_coder *coder, uint8_t *result);
-
-// /**
-//  * @brief Read 8-bit value of P1.01 - P1.08
-//  *
-//  * This function performs a read operation of
-//  * pins 1-8 of port 1. The result contains the 8-bit
-//  * representation of these pins.
-//  *
-//  * @param coder   	  io_coder object.
-//  * @param result      Pointer in which the read value will be stored.
-//  *
-//  * @return Zero on success or (negative) error code otherwise.
-//  */
-// int read_P1_01_to_P1_08(struct io_coder *coder, uint8_t *result);
+/**
+ * @brief Write 8-bit value
+ * 
+ * This function performs a write operation to the set pin range
+ * and port specified in the io_coder object.
+ *
+ * @param coder   	  io_coder object.
+ * @param value       uint8_t which gets written to the set pin range
+ *
+ * @return Zero on success or (negative) error code otherwise.
+ */
+int write_8_bit(struct io_coder *coder, uint8_t value);
 
 #endif
--- samples/gatcode/8bit_gpio/src/io_coder.c
+++ samples/gatcode/8bit_gpio/src/io_coder.c
@@ -1,43 +1,24 @@
 #include <io_coder.h>
 
-int set_port(struct io_coder *coder, const struct device *port)
+int setup_8_bit_io(struct io_coder *coder, uint8_t pins[8], bool port1, bool read)
 {
-    if(coder == NULL || port == NULL) {
+    if(coder == NULL) {
         return -EINVAL;
     }
 
-    coder->port = port;
-    return 0;
-}
-
-int setup_pin(struct io_coder *coder, uint8_t pin, bool read)
-{
-    if(coder->port == NULL) {
-        return -EINVAL;
+    if(port1) {
+        coder->port = device_get_binding("GPIO_1");
+    } else {
+        coder->port = device_get_binding("GPIO_0");
     }
-
-    int err = gpio_pin_configure(coder->port, pin, read ? GPIO_INPUT : GPIO_OUTPUT);
-    if(err) {
-        return err;
-    }
-
-    return 0;
-}
-
-int setup_8_bit_io(struct io_coder *coder, uint8_t first_pin, uint8_t last_pin, bool read)
-{
-    if(coder == NULL || first_pin > last_pin || last_pin - first_pin != 7 || first_pin < 0 || last_pin < 0) {
-        return -EINVAL;
-    }
-
-    coder->port = device_get_binding("GPIO_1");
     if(coder->port == NULL) {
         return -EINVAL;
     }
 
     int err;
-    for(uint8_t i = first_pin; i <= last_pin; i++) {
-		err = setup_pin(coder, i, read);
+    for(uint8_t i = 0; i < 8; i++) {
+        coder->pins |= BIT(pins[i]);
+		err = gpio_pin_configure(coder->port, pins[i], read ? GPIO_INPUT : GPIO_OUTPUT);
         if(err) {
             return err;
         }
@@ -46,8 +27,27 @@ int setup_8_bit_io(struct io_coder *coder, uint8_t first_pin, uint8_t last_pin, 
     return 0;
 }
 
-int read_8_bit(struct io_coder *coder, uint8_t first_pin, uint8_t last_pin, uint8_t *result)
+int setup_8_bit_io_consecutive(struct io_coder *coder, uint8_t first_pin, uint8_t last_pin, bool port1, bool read)
 {
+    if(coder == NULL || first_pin > last_pin || last_pin - first_pin != 7 || first_pin < 0 || last_pin < 0) {
+        return -EINVAL;
+    }
+
+    uint8_t pins[8];
+    uint8_t index = 0;
+    for(uint8_t pin = first_pin; pin <= last_pin; pin++) {
+		pins[index++] = pin;
+	}
+
+    return setup_8_bit_io(coder, pins, port1, read);
+}
+
+int read_8_bit(struct io_coder *coder, uint8_t *result)
+{
+    if(coder == NULL || result == NULL) {
+        return -EINVAL;
+    }
+
     gpio_port_value_t value;
     
     int err = gpio_port_get(coder->port, &value);
@@ -58,13 +58,19 @@ int read_8_bit(struct io_coder *coder, uint8_t first_pin, uint8_t last_pin, uint
     #ifdef IO_CODER_ACTIVE_LEVEL_LOW
         *result = ~value >> 1;
     #else
-        uint32_t mask = 0;
-        for (uint8_t i = first_pin; i <= last_pin; i++)
-        {
-            mask |= ~BIT(i);
-        }
-        *result = (value & mask) >> 1;
+        *result = (value & coder->pins) >> 1;
     #endif
+
+    return 0;
+}
+
+int write_8_bit(struct io_coder *coder, uint8_t value)
+{
+    if(coder == NULL) {
+        return -EINVAL;
+    }
+
+    gpio_port_set_clr_bits(coder->port, value << 1, ~(value << 1));
 
     return 0;
 }
