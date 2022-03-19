a61b65f643246c3beaf1e7a480c565d6b7f9b4be
GatCode <gatcode@wdw.one>
2022-03-19 14:34:58 +0100

    Finish 8bit IO Coder

--- samples/gatcode/8bit_gpio/src/main.c
+++ samples/gatcode/8bit_gpio/src/main.c
@@ -3,60 +3,42 @@
 
 #include <io_coder.h>
 
-static struct io_coder coder = {0};
-
-const struct device *port1;
-
-// void main(void)
-// {
-//     port1 = device_get_binding("GPIO_1");
-
-// 	gpio_pin_configure(port1, 1, GPIO_OUTPUT_LOW);
-// 	gpio_pin_configure(port1, 2, GPIO_OUTPUT_LOW);
-// 	gpio_pin_configure(port1, 3, GPIO_OUTPUT_LOW);
-// 	gpio_pin_configure(port1, 4, GPIO_OUTPUT_LOW);
-// 	gpio_pin_configure(port1, 5, GPIO_OUTPUT_LOW);
-// 	gpio_pin_configure(port1, 6, GPIO_OUTPUT_LOW);
-// 	gpio_pin_configure(port1, 7, GPIO_OUTPUT_LOW);
-// 	gpio_pin_configure(port1, 8, GPIO_OUTPUT_LOW);
-
-//     gpio_port_pins_t mask = BIT(1) | BIT(2);
-//     int err = gpio_port_set_masked(port1, mask, 1);
-//     if(err) {
-//         printk("Error setting gpio_port_set_masked (err %d)\n", err);
-//     }
-
-//     while(1) {
-// 		gpio_port_pins_t set = BIT(1) | BIT(3) | BIT(5) | BIT(7);
-// 	  	gpio_port_pins_t clear = BIT(2) | BIT(4) | BIT(6) | BIT(8);
-
-// 		gpio_port_set_clr_bits(port1, set, clear);
-// 		k_sleep(K_MSEC(1000));
-
-// 		gpio_port_set_clr_bits(port1, clear, set);
-// 		k_sleep(K_MSEC(1000));
-// 	}
-// }
-
+static struct io_coder io_encoder = {0};
+static struct io_coder io_decoder = {0};
 
 void main(void)
 {
 	int err;
 	uint8_t reading;
 
-	err = setup_8_bit_io(&coder, 1, 8, true);
+	/* Setup Encoder Pins */
+	err = setup_8_bit_io_consecutive(&io_encoder, 1, 8, true, false);
 	if(err) {
 		printk("Error setting up P1.01 - P1.08 (err %d)\n", err);
 	}
 
+	/* Setup Decoder Pins */
+	// uint8_t pins[8] = {1, 2, 3, 4, 5, 6, 7, 8};
+	// err = setup_8_bit_io(&io_decoder, (uint8_t*)&pins, true, true);
+	// if(err) {
+	// 	printk("Error setting up P1.01 - P1.08 (err %d)\n", err);
+	// }
+
 	while(1) {
-		err = read_8_bit(&coder, 1, 8, &reading);
-		if(err) {
-			printk("Error reading 8bit value from P1.01 - P1.08 (err %d)\n", err);
+		for (uint8_t i = 0; i < 256; i++)
+		{
+			err = write_8_bit(&io_encoder, i);
+			if(err) {
+				printk("Error writing 8bit value to P1.01 - P1.08 (err %d)\n", err);
+			}
+
+			// err = read_8_bit(&io_decoder, &reading);
+			// if(err) {
+			// 	printk("Error reading 8bit value from P1.01 - P1.08 (err %d)\n", err);
+			// }
+			// printk("READ: %u\n", reading);
+
+			k_sleep(K_MSEC(1000));
 		}
-
-		printk("P1 1-8: %u\n", reading);
-
-		k_msleep(1000);
 	}
 }
