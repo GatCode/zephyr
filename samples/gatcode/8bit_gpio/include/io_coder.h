b816b08859266904088dcb795445fcf7bc1ce227
GatCode <gatcode@wdw.one>
2022-03-19 12:03:31 +0100

    Refactor

--- samples/gatcode/8bit_gpio/include/io_coder.h
+++ samples/gatcode/8bit_gpio/include/io_coder.h
@@ -7,122 +7,126 @@
 /**
  * If enabled, the logic operates in active low fashion
  */
-#define IO_CODER_ACTIVE_LEVEL_LOW
+// #define IO_CODER_ACTIVE_LEVEL_LOW
 
 struct io_coder {
 	/**
-	 * @brief device* containing port0.
+	 * @brief device* containing the port.
 	 */
-	const struct device *port0;
-	/**
-	 * @brief device* containing port1.
-	 */
-	const struct device *port1;
+	const struct device *port;
 };
+
+/**
+ * @brief Set specific GPIO port
+ *
+ * This function sets the io_coder port to the given port.
+ *
+ * @param coder   	  io_coder object.
+ * @param port    	  Pointer to a GPIO port.
+ *
+ * @return Zero on success or (negative) error code otherwise.
+ */
+int set_port(struct io_coder *coder, const struct device *port);
 
 /**
  * @brief Pin Setup
  *
- * This function performs the setup of a given pin
- * to a given port.
+ * This function performs the setup of a given pin.
  *
- * @param port   	  Port of the MCU.
+ * @param coder   	  io_coder object.
  * @param pin    	  Pin number of the MCU.
+ * @param read        Toggle for the read mode configuration.
  *
  * @return Zero on success or (negative) error code otherwise.
  */
-int setup_pin(const struct device *port, uint8_t pin);
+int setup_pin(struct io_coder *coder, uint8_t pin, bool read);
 
 /**
- * @brief Setup 8 consecutive pins
- *
+ * @brief Pin setup of 8 given pins for port 1
+ * 
  * This function performs the setup of 8 consecutive pins
- * to a given port (e.g. P0.01-P0.08).
+ * of port 1 in read or write mode.
  *
- * @param port   	  Port of the MCU.
+ * @param coder   	  io_coder object.
  * @param first_pin   Pin number first pin.
  * @param last_pin    Pin number first pin.
+ * @param read   	  Read mode configuration flag.
  *
  * @return Zero on success or (negative) error code otherwise.
  */
-int setup_8_consecutive_pins(const struct device *port, uint8_t first_pin, uint8_t last_pin);
+int setup_8_bit_io(struct io_coder *coder, uint8_t first_pin, uint8_t last_pin, bool read);
 
 /**
- * @brief Pin Setup of pins 1-8 for a given port
- *
- * This function performs the setup of pins 1-8
- * for a given port.
- *
- * @param port   	  Port of the MCU.
- *
- * @return Zero on success or (negative) error code otherwise.
- */
-int setup_P0_01_to_P0_08_from_port(const struct device *port);
-
-/**
- * @brief Pin Setup of pins 1-8 for the port 0
- *
- * This function performs the setup of pins 1-8
- * for port 0 of the MCU.
+ * @brief Read 8-bit value of the given pin range
+ * 
+ * This function performs a read operation of the given pin range
+ * on the port specified in the io_coder object. The result contains
+ * the 8-bit representation of of the pin states.
  *
  * @param coder   	  io_coder object.
- *
- * @return Zero on success or (negative) error code otherwise.
- */
-int setup_P0_01_to_P0_08(struct io_coder *coder);
-
-/**
- * @brief Pin Setup of pins 1-8 for the port 1
- *
- * This function performs the setup of pins 1-8
- * for port 1 of the MCU.
- *
- * @param coder   	  io_coder object.
- *
- * @return Zero on success or (negative) error code otherwise.
- */
-int setup_P1_01_to_P1_08(struct io_coder *coder);
-
-/**
- * @brief Read 8-bit value of the pins 1-8 from a given port
- *
- * This function performs a read operation of
- * pins 1-8 of the given port. The result contains the 8-bit
- * representation of these pins.
- *
- * @param port   	  Port of the MCU.
+ * @param first_pin   Pin number first pin.
+ * @param last_pin    Pin number first pin.
  * @param result      Pointer in which the read value will be stored.
  *
  * @return Zero on success or (negative) error code otherwise.
  */
-int read_P01_to_P08_from_port(const struct device *port, uint8_t *result);
+int read_8_bit(struct io_coder *coder, uint8_t first_pin, uint8_t last_pin, uint8_t *result);
 
-/**
- * @brief Read 8-bit value of P0.01 - P0.08
- *
- * This function performs a read operation of
- * pins 1-8 of port 0. The result contains the 8-bit
- * representation of these pins.
- *
- * @param coder   	  io_coder object.
- * @param result      Pointer in which the read value will be stored.
- *
- * @return Zero on success or (negative) error code otherwise.
- */
-int read_P0_01_to_P0_08(struct io_coder *coder, uint8_t *result);
 
-/**
- * @brief Read 8-bit value of P1.01 - P1.08
- *
- * This function performs a read operation of
- * pins 1-8 of port 1. The result contains the 8-bit
- * representation of these pins.
- *
- * @param coder   	  io_coder object.
- * @param result      Pointer in which the read value will be stored.
- *
- * @return Zero on success or (negative) error code otherwise.
- */
-int read_P1_01_to_P1_08(struct io_coder *coder, uint8_t *result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// /**
+//  * @brief Read 8-bit value of the pins 1-8 from a given port
+//  *
+//  * This function performs a read operation of
+//  * pins 1-8 of the given port. The result contains the 8-bit
+//  * representation of these pins.
+//  *
+//  * @param port   	  Port of the MCU.
+//  * @param result      Pointer in which the read value will be stored.
+//  *
+//  * @return Zero on success or (negative) error code otherwise.
+//  */
+// int read_P01_to_P08_from_port(const struct device *port, uint8_t *result);
+
+// /**
+//  * @brief Read 8-bit value of P0.01 - P0.08
+//  *
+//  * This function performs a read operation of
+//  * pins 1-8 of port 0. The result contains the 8-bit
+//  * representation of these pins.
+//  *
+//  * @param coder   	  io_coder object.
+//  * @param result      Pointer in which the read value will be stored.
+//  *
+//  * @return Zero on success or (negative) error code otherwise.
+//  */
+// int read_P0_01_to_P0_08(struct io_coder *coder, uint8_t *result);
+
+// /**
+//  * @brief Read 8-bit value of P1.01 - P1.08
+//  *
+//  * This function performs a read operation of
+//  * pins 1-8 of port 1. The result contains the 8-bit
+//  * representation of these pins.
+//  *
+//  * @param coder   	  io_coder object.
+//  * @param result      Pointer in which the read value will be stored.
+//  *
+//  * @return Zero on success or (negative) error code otherwise.
+//  */
+// int read_P1_01_to_P1_08(struct io_coder *coder, uint8_t *result);
 
 #endif
--- samples/gatcode/8bit_gpio/src/main.c
+++ samples/gatcode/8bit_gpio/src/main.c
@@ -3,70 +3,60 @@
 
 #include <io_coder.h>
 
-static struct io_coder io_8bit_coder = {0};
+static struct io_coder coder = {0};
 
 const struct device *port1;
 
-void main(void)
-{
-    port1 = device_get_binding("GPIO_1");
-
-	gpio_pin_configure(port1, 1, GPIO_OUTPUT_LOW);
-	gpio_pin_configure(port1, 2, GPIO_OUTPUT_LOW);
-	gpio_pin_configure(port1, 3, GPIO_OUTPUT_LOW);
-	gpio_pin_configure(port1, 4, GPIO_OUTPUT_LOW);
-	gpio_pin_configure(port1, 5, GPIO_OUTPUT_LOW);
-	gpio_pin_configure(port1, 6, GPIO_OUTPUT_LOW);
-	gpio_pin_configure(port1, 7, GPIO_OUTPUT_LOW);
-	gpio_pin_configure(port1, 8, GPIO_OUTPUT_LOW);
-
-    gpio_port_pins_t mask = BIT(1) | BIT(2);
-    int err = gpio_port_set_masked(port1, mask, 1);
-    if(err) {
-        printk("Error setting gpio_port_set_masked (err %d)\n", err);
-    }
-
-    while(1) {
-		gpio_port_pins_t set = BIT(1) | BIT(3) | BIT(5) | BIT(7);
-	  	gpio_port_pins_t clear = BIT(2) | BIT(4) | BIT(6) | BIT(8);
-
-		gpio_port_set_clr_bits(port1, set, clear);
-		k_sleep(K_MSEC(1000));
-
-		gpio_port_set_clr_bits(port1, clear, set);
-		k_sleep(K_MSEC(1000));
-	}
-}
-
-
 // void main(void)
 // {
-// 	int err;
-// 	uint8_t p0_01_to_p0_08_reading, p1_01_to_p1_08_reading;
+//     port1 = device_get_binding("GPIO_1");
 
-// 	err = setup_P0_01_to_P0_08(&io_8bit_coder);
-// 	if(err) {
-// 		printk("Error setting up P1.01 - P1.08 (err %d)\n", err);
-// 	}
+// 	gpio_pin_configure(port1, 1, GPIO_OUTPUT_LOW);
+// 	gpio_pin_configure(port1, 2, GPIO_OUTPUT_LOW);
+// 	gpio_pin_configure(port1, 3, GPIO_OUTPUT_LOW);
+// 	gpio_pin_configure(port1, 4, GPIO_OUTPUT_LOW);
+// 	gpio_pin_configure(port1, 5, GPIO_OUTPUT_LOW);
+// 	gpio_pin_configure(port1, 6, GPIO_OUTPUT_LOW);
+// 	gpio_pin_configure(port1, 7, GPIO_OUTPUT_LOW);
+// 	gpio_pin_configure(port1, 8, GPIO_OUTPUT_LOW);
 
-// 	err = setup_P1_01_to_P1_08(&io_8bit_coder);
-// 	if(err) {
-// 		printk("Error setting up P1.01 - P1.08 (err %d)\n", err);
-// 	}
+//     gpio_port_pins_t mask = BIT(1) | BIT(2);
+//     int err = gpio_port_set_masked(port1, mask, 1);
+//     if(err) {
+//         printk("Error setting gpio_port_set_masked (err %d)\n", err);
+//     }
 
-// 	while(1) {
-// 		err = read_P0_01_to_P0_08(&io_8bit_coder, &p0_01_to_p0_08_reading);
-// 		if(err) {
-// 			printk("Error reading 8bit value from P1.01 - P1.08 (err %d)\n", err);
-// 		}
+//     while(1) {
+// 		gpio_port_pins_t set = BIT(1) | BIT(3) | BIT(5) | BIT(7);
+// 	  	gpio_port_pins_t clear = BIT(2) | BIT(4) | BIT(6) | BIT(8);
 
-// 		err = read_P1_01_to_P1_08(&io_8bit_coder, &p1_01_to_p1_08_reading);
-// 		if(err) {
-// 			printk("Error reading 8bit value from P1.01 - P1.08 (err %d)\n", err);
-// 		}
+// 		gpio_port_set_clr_bits(port1, set, clear);
+// 		k_sleep(K_MSEC(1000));
 
-// 		printk("P0 1-8: %u | P1 1-8: %u\n", p0_01_to_p0_08_reading, p1_01_to_p1_08_reading);
-
-// 		k_msleep(1000);
+// 		gpio_port_set_clr_bits(port1, clear, set);
+// 		k_sleep(K_MSEC(1000));
 // 	}
 // }
+
+
+void main(void)
+{
+	int err;
+	uint8_t reading;
+
+	err = setup_8_bit_io(&coder, 1, 8, true);
+	if(err) {
+		printk("Error setting up P1.01 - P1.08 (err %d)\n", err);
+	}
+
+	while(1) {
+		err = read_8_bit(&coder, 1, 8, &reading);
+		if(err) {
+			printk("Error reading 8bit value from P1.01 - P1.08 (err %d)\n", err);
+		}
+
+		printk("P1 1-8: %u\n", reading);
+
+		k_msleep(1000);
+	}
+}
